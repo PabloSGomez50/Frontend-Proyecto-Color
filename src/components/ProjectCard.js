48592bc431345fc2d15bfd0665cf89fd33392c54
Pablo Gomez <pablosgomez50@gmail.com>
2022-05-14 23:23:15 -0300

    1. Create api and components folder

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProjectCard = props => {
+    const data = props;
+    if (data) {
+        console.log("works");
+    }
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProjectCard;
